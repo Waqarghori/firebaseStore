55bad9c1ec858aca084f55f3e79af8cbf4c7f45e
Waqarghori <waqar.iqbal789@gmail.com>
2025-08-02 09:32:13 +0500

    upd

---
+++ app.js
@@ -0,0 +1,36 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+  import { getFirestore ,collection, addDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDhdCQAdSrBaGdbVeELnfVCKzEXsgxpo_c",
+    authDomain: "zenovategym.firebaseapp.com",
+    projectId: "zenovategym",
+    storageBucket: "zenovategym.firebasestorage.app",
+    messagingSenderId: "26538049420",
+    appId: "1:26538049420:web:7da2e9c395de5a840320a9",
+    measurementId: "G-0RYR5233ZV"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+
+let userBtn = document.getElementById('sbtn')
+userBtn.addEventListener('click', async () => {
+    let name = document.getElementById('userName').value
+    let email = document.getElementById('userEmail').value
+    let age = document.getElementById('userAge').value
+    
+
+try {
+  const docRef = await addDoc(collection(db, "users"), {
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+  });
+  console.log("Document written with ID: ", docRef.id);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
+
+})
