7685eee57a5970dc0af9672fdb36ec8c58f9c7cb
Waqarghori <waqar.iqbal789@gmail.com>
2025-08-02 18:46:06 +0500

    upd

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
-  
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getFirestore ,collection, addDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDhdCQAdSrBaGdbVeELnfVCKzEXsgxpo_c",
-    authDomain: "zenovategym.firebaseapp.com",
-    projectId: "zenovategym",
-    storageBucket: "zenovategym.firebasestorage.app",
-    messagingSenderId: "26538049420",
-    appId: "1:26538049420:web:7da2e9c395de5a840320a9",
-    measurementId: "G-0RYR5233ZV"
-  };
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+const firebaseConfig = {
+  apiKey: "AIzaSyDhdCQAdSrBaGdbVeELnfVCKzEXsgxpo_c",
+  authDomain: "zenovategym.firebaseapp.com",
+  projectId: "zenovategym",
+  storageBucket: "zenovategym.firebasestorage.app",
+  messagingSenderId: "26538049420",
+  appId: "1:26538049420:web:7da2e9c395de5a840320a9",
+  measurementId: "G-0RYR5233ZV"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 
 let userBtn = document.getElementById('sbtn')
 userBtn.addEventListener('click', async () => {
-    let name = document.getElementById('userName').value
-    let email = document.getElementById('userEmail').value
-    let age = document.getElementById('userAge').value
-    
+  let name = document.getElementById('userName').value
+  let email = document.getElementById('userEmail').value
+  let age = document.getElementById('userAge').value
 
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    name,
-    email,
-    age
-  });
-  console.log("Document written with ID: ", docRef.name);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
 
-name = ''
-email = ''
-age = ''
+  try {
+    const docRef = await addDoc(collection(db, "clint"), {
+      name,
+      email,
+      age
+    });
+    console.log("Document written with ID: ", docRef.id);
+    document.getElementById('userName').value = ''
+    document.getElementById('userEmail').value = ''
+    document.getElementById('userAge').value = ''
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+
+
 })
